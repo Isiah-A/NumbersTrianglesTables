b0582dc5fcbed52f970350711fcfc350182f95f5
isiah-a <isiaha478@gmail.com>
2025-03-03 20:30:09 -0500

    NumberUtilities test passed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,18 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        return null; //build for loop that goes from 1 to 5 then
+        // 1-5
+        // 2-4
+        // 3-15
+        // 5-25
     }
 
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 }
